b995631af474a6048872f9ae22b9916560c1185e
cynic <daniel@szymanski.cc>
2024-12-01 08:28:36 +0100

    Add day 01

--- 01/src/main.rs
+++ 01/src/main.rs
@@ -1,25 +1,45 @@
-use regex::Regex;
+use std::collections::HashMap;
 use std::{
     fs::File,
     io::{self, BufRead},
+    iter::zip,
 };
 
 fn main() {
-    let filename = "input1.txt";
+    let filename = "input2.txt";
     let file = File::open(filename).unwrap();
-    let lines = io::BufReader::new(file).lines();
-    let re = Regex::new(r"(\d)").unwrap();
+    let lines: Vec<_> = io::BufReader::new(file).lines().collect::<Result<_, _>>().unwrap();
 
-    let sum = lines
-        .map(|line| {
-            let line = line.unwrap();
-            //println!("{}", line);
-            let captures = re.captures_iter(&line);
-            let values: Vec<i32> = captures.map(|cap| cap[1].parse::<i32>().unwrap()).collect();
-            let entry = values.first().unwrap() * 10 + values.last().unwrap();
-            entry
-        })
-        .sum::<i32>();
+    let mut left: Vec<i32> = Vec::new();
+    let mut right: Vec<i32> = Vec::new();
+    lines.iter().for_each(|line| {
+        //println!("'{}'", line);
+        let splits = line.split("  ").collect::<Vec<&str>>();
+        left.push(splits[0].trim().parse::<i32>().unwrap());
+        right.push(splits[1].trim().parse::<i32>().unwrap());
+    });
+    left.sort();
+    right.sort();
 
-    println!("sum: {}", sum);
+    let mut dists = 0;
+    zip(left.iter(), right.iter()).for_each(|(l, r)| {
+        //println!("{} {}", l, r);
+        dists += (r - l).abs();
+    });
+
+    println!("part1: {}", dists);
+
+    let right_map = right.iter().fold(HashMap::new(), |mut acc, c| {
+        *acc.entry(c).or_insert(0) += 1;
+        acc
+    });
+    //println!("{:?}", right_map);
+
+    let mut score = 0;
+    left.iter().for_each(|l| {
+        if let Some(count) = right_map.get(l) {
+            score += l * count;
+        }
+    });
+    println!("part2: {}", score);
 }
