200826809483e0cc040e3213a855becbaf72a844
cynic <daniel@szymanski.cc>
2024-12-22 23:22:26 +0100

    Some cleanup for day 22 part 2
    But no algorithmic optimization

--- 22/src/main.rs
+++ 22/src/main.rs
@@ -1,4 +1,3 @@
-use std::cmp::max;
 use std::collections::{HashMap, HashSet};
 use std::fs;
 
@@ -12,7 +11,7 @@ fn main() {
         .iter()
         .map(|v| {
             let mut v = *v;
-            for n in 0..2000 {
+            for _ in 0..2000 {
                 v = process(v);
             }
             v
@@ -22,22 +21,22 @@ fn main() {
 
     // part 2
     // let values: Vec<i64> = vec![1, 2, 3, 2024];
+    let bananas = part2(&values);
+    println!("Part 2: {}", bananas);
+}
+
+fn part2(values: &Vec<i64>) -> i64 {
     let mut global_unique_sequences: HashSet<[i64; 4]> = HashSet::new();
     let mut unique_sequences_per_secret_number: Vec<HashMap<[i64; 4], i64>> = Vec::new();
-    for i in 0..values.len() {
+    (0..values.len()).for_each(|i| {
         let mut v = values[i];
-        let mut diffs = vec![];
-        let mut prices = vec![];
-        for n in 0..2000 {
-            let new_v = process(v);
-            let rs = v.to_string();
-            let prev_price = rs[rs.len() - 1..].parse::<i64>().unwrap();
-            let ls = new_v.to_string();
-            let new_price = ls[ls.len() - 1..].parse::<i64>().unwrap();
-            prices.push(prev_price);
-            diffs.push(new_price - prev_price);
-            v = new_v;
-        }
+        let (prices, diffs): (Vec<i64>, Vec<i64>) = (0..2000)
+            .map(|_| {
+                let prev_price = v % 10;
+                v = process(v);
+                (prev_price, v % 10 - prev_price)
+            })
+            .collect();
         let mut unique_sequences: HashMap<[i64; 4], i64> = HashMap::new();
         for n in 0..diffs.len() - 4 {
             let s = &diffs[n..n + 4];
@@ -50,19 +49,18 @@ fn main() {
             global_unique_sequences.insert(*k);
         }
         unique_sequences_per_secret_number.push(unique_sequences);
-    }
-
-    let mut bananas = 0;
-    global_unique_sequences.iter().for_each(|seq| {
-        let mut local_bananas = 0;
-        unique_sequences_per_secret_number.iter().for_each(|unique_sequences| {
-            if let Some(v) = unique_sequences.get(seq) {
-                local_bananas += v;
-            }
-        });
-        bananas = max(bananas, local_bananas);
     });
-    println!("Part 2: {}", bananas);
+
+    global_unique_sequences
+        .iter()
+        .map(|glob_seq| {
+            unique_sequences_per_secret_number
+                .iter()
+                .map(|secret_seq| secret_seq.get(glob_seq).unwrap_or(&0))
+                .sum()
+        })
+        .max()
+        .unwrap()
 }
 
 fn process(v: i64) -> i64 {
@@ -70,11 +68,4 @@ fn process(v: i64) -> i64 {
     let v = ((v >> 5) ^ v) % 16777216;
     let v = ((v << 11) ^ v) % 16777216;
     v
-}
-
-fn find_subsequence<T>(container: &[T], sequence: &[T]) -> Option<usize>
-where
-    for<'a> &'a [T]: PartialEq,
-{
-    container.windows(sequence.len()).position(|window| window == sequence)
 }
